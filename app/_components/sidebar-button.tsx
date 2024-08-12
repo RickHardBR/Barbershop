73ef4628fc10ea329ad37d0c4ccb1d54c87572c6
Seu Nome <rickhard@bol.com.br>
2024-08-12 00:28:39 -0300

    chore: add next auth setup

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image"
 import { Button } from "./ui/button"
-import { CalendarDays, HomeIcon, LogOut, MenuIcon } from "lucide-react"
+import {
+  CalendarDays,
+  HomeIcon,
+  LogInIcon,
+  LogOut,
+  MenuIcon,
+} from "lucide-react"
 import {
   Sheet,
   SheetClose,
@@ -10,8 +16,16 @@ import {
   SheetTrigger,
 } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarButton = () => {
   return (
@@ -27,8 +41,8 @@ const SidebarButton = () => {
         </SheetHeader>
 
         {/* CABEÇALHO */}
-        <div className="flex items-center gap-3 border-b border-solid py-5">
-          <Avatar>
+        <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+          {/* <Avatar>
             <AvatarImage
               className="h-12 w-12 rounded-3xl border-2 border-solid border-purple-400"
               src="https://s3-alpha-sig.figma.com/img/0253/a980/873ac68aebe436f0d55d07b08c2901da?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=OQIQN6LbpPBvz3xRBPgwRFpK0jx2H3h-FddZBT4AgnE3hxbayoXmA8WPLnGqP1GjaCEVmBFFnhh7UtMI7p326snSVhc1291UzfJax1RgRjxabwKwnhu4fAeOTnnWJbrFslhrfTsDhQlsQ-WYxeBPzjBhbZbbyM~GfE~uvwSF-0jP0GZLZgijm1MOlPAmC1NQs~D05~XuqFpDNkqW7PmS8oJrI3sRihDYlJhmvEvueov2MjHcmcdIIzxyrmtkqLaMJYxckKZIJIFiH0TsF0UU0fpr~YlxmX-RY175DrchCN6ko8d3C1fK7pZislA7RPq0KGuAAUY7P2OO5USmkDjgYA__ "
@@ -38,7 +52,32 @@ const SidebarButton = () => {
           <div className="ml-2">
             <h2 className="mb-1 font-bold">Ricardo Ribeiro</h2>
             <p className="text-xs">rickhard@bol.com.br</p>
-          </div>
+          </div> */}
+          <h2 className="font-bold">Olá, faça seu login!</h2>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size={"icon"}>
+                <LogInIcon />
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="w-[90%]">
+              <DialogHeader>
+                <DialogTitle>Faça seu login na plataforma</DialogTitle>
+                <DialogDescription>
+                  Conecte-se usando sua conta Google.
+                </DialogDescription>
+              </DialogHeader>
+              <Button variant="outline" className="gap-2 font-bold">
+                <Image
+                  alt="Logo do Google"
+                  src="/googleicon.svg"
+                  width={18}
+                  height={18}
+                />
+                Google
+              </Button>
+            </DialogContent>
+          </Dialog>
         </div>
 
         {/* INICIO */}
